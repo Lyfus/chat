83861ba580169dab20aca298003a80494eae3114
SimonS <savattiersimon@gmail.com>
2019-01-11 10:42:48 +0100

    Ajout Class Users

---
+++ Chat/Chat/Classes/Messages.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.Classes
+{
+    class Messages
+    {
+        public int Id { get; set; }
+        DateTime PubDate { get; set; }
+        string Content { get; set; }
+        User User { get; set; }
+
+        public Messages(int id, DateTime pubDate, string content, User user)
+        {
+            Id = id;
+            PubDate = pubDate;
+            Content = content;
+            User = user;
+        }
+    }
+}
