399b43cf9b7956f8bf0f33cfbda2053a34e6372a
Hellennwell <quentin.daniel0727@gmail.com>
2019-01-11 11:24:00 +0100

    User class

---
+++ Chat/Chat/Classes/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.Classes
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Pseudo { get; set; }
+        public string Password { get; set; }
+        private List<Group> _groups = new List<Group>();
+        public List<Group> Groups { get { return _groups; } }
+
+        public User(int id, string pseudo, string password)
+        {
+            Id = id;
+            Pseudo = pseudo;
+            Password = password;
+        }
+    }
+}
