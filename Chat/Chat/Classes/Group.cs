d6668917d7fd861bd346e285026b52ca7a52b09a
SimonS <savattiersimon@gmail.com>
2019-01-11 10:36:11 +0100

    Ajout classes + Group

---
+++ Chat/Chat/Classes/Group.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.Classes
+{
+    class Group
+    {
+        public int Id { get; set; }
+        public int Description { get; set; }
+        private List<User> _users = new List<User>();
+        public List<User> Users { get { return _users; } }
+
+        public Group(int id, int description)
+        {
+            Id = id;
+            Description = description;
+        }
+    }
+}
